6d0c360184fbca1b054a840b615fb78f46b3fa23
Joakim Saario <s@xyr.se>
2014-12-21 22:35:41 +0100

    log exceptions but don't crash

--- main.py
+++ main.py
@@ -4,12 +4,26 @@ from state import State, from_dict
 import irc, network
 
 
+logger = logging.getLogger(__name__)
+
+
+def log_exceptions(f):
+    def new_f(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            logger.error(e)
+            return f(*args, **kwargs)
+
+    return new_f
+
 
 def load_settings():
     with open('settings.json', 'r') as conf:
         return from_dict(json.load(conf))
 
 
+@log_exceptions
 def init():
     state = State()
     state.settings = load_settings()
@@ -17,15 +31,22 @@ def init():
                                   state.settings.irc.port,
                                   ssl=state.settings.irc.ssl)
 
-    # set up logging
-    logging.basicConfig(level=logging.DEBUG)
-
     irc.init(sock, state)
+
+    return sock, state
+
+
+@log_exceptions
+def main_loop(sock, state):
     while True:
         irc.get_message(sock, state)
 
 
 if __name__ == '__main__':
-    init()
+    # set up logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    sock, state = init()
+    main_loop(sock, state)
 
 
