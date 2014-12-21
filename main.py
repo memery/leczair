9007744e70d829c11c4fc0e60ad2a4b9e42c036b
Joakim Saario <s@xyr.se>
2014-12-21 23:23:07 +0100

    using contextmanager instead of decorator

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json, logging
+from contextlib import contextmanager
 
 from state import State, from_dict
 import irc, network
@@ -7,15 +8,13 @@ import irc, network
 logger = logging.getLogger(__name__)
 
 
-def log_exceptions(f):
-    def new_f(*args, **kwargs):
-        while True:
-            try:
-                return f(*args, **kwargs)
-            except Exception as e:
-                logger.error(e)
-
-    return new_f
+@contextmanager
+def log_exceptions():
+    try:
+        yield
+    except Exception as e:
+        # TODO: Make new logger so that the right module name is used.
+        logger.error(e)
 
 
 def load_settings():
@@ -23,30 +22,23 @@ def load_settings():
         return from_dict(json.load(conf))
 
 
-@log_exceptions
 def init():
-    state = State()
-    state.settings = load_settings()
-    sock = network.BufferedSocket(state.settings.irc.host,
-                                  state.settings.irc.port,
-                                  ssl=state.settings.irc.ssl)
-
-    irc.init(sock, state)
-
-    return sock, state
-
-
-@log_exceptions
-def main_loop(sock, state):
-    while True:
-        irc.get_message(sock, state)
-
-
-if __name__ == '__main__':
     # set up logging
     logging.basicConfig(level=logging.DEBUG)
 
-    sock, state = init()
-    main_loop(sock, state)
+    with log_exceptions():
+        state = State()
+        state.settings = load_settings()
+        sock = network.BufferedSocket(state.settings.irc.host,
+                                      state.settings.irc.port,
+                                      ssl=state.settings.irc.ssl)
+
+        irc.init(sock, state)
+
+        while True:
+            with log_exceptions():
+                irc.get_message(sock, state)
 
 
+if __name__ == '__main__':
+    init()
