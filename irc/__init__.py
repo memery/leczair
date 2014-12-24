9bd2d6c311dfe09b3a8efbb696d3c6acd38e4f0a
kqr <k@rdw.se>
2014-12-24 13:20:35 +0100

    refactored the main loop thingey

--- irc/__init__.py
+++ irc/__init__.py
@@ -1,9 +1,6 @@
 import random, string
-
 from logging import getLogger
-
-import state
-
+import network
 from .parser import Message, parse_privmsg, get_nick
 from .render import to_raw
 
@@ -11,63 +8,55 @@ from .render import to_raw
 logger = getLogger(__name__)
 
 
-def init(sock, state):
-    irc_settings = state.settings.irc
-    send_message(sock, Message(command='NICK', arguments=[irc_settings.nick]))
+def create_socket(irc):
+    return network.BufferedSocket(irc.host, irc.port, ssl=irc.ssl)
 
-    user_arguments = [irc_settings.nick, '0', '*',
-                     'IRC bot {}'.format(irc_settings.nick)]
+
+def hello(sock, settings, state):
+    send_message(sock, Message(command='NICK', arguments=[settings.nick]))
+
+    user_arguments = [settings.nick, '0', '*', 'Bot {}'.format(settings.nick)]
     send_message(sock, Message(command='USER', arguments=user_arguments))
 
-    state.irc.nick = irc_settings.nick
-    state.irc.joined = False
-    state.irc.registered = False
+    state.nick = settings.nick
+    state.joined = ''
 
 
-def manage(message, state):
+def manage(message, settings, state):
     if message.command == 'PING':
         return Message(command='PONG', arguments=message.arguments)
     elif message.command == '403':
         # Channel doesn't exist, stop trying to join
-        state.settings.irc.channel = None
+        settings.channel = None
     elif message.command == '433':
-        def new_nick_proposal(nick):
-            # determine how much to shave off to make room for random chars
-            nick = nick[:min(len(nick), 6)]
+        def generate_nick(nick):
+            truncated = nick[:min(len(nick), 6)]
+            alphabet = string.ascii_lowercase + string.digits
+            random_chars = ''.join(random.choice(alphabet) for _ in range(2))
+            return '{}_{}'.format(truncated, random_chars)
 
-            random_choices = string.ascii_lowercase + string.digits
-            random_chars = ''.join(random.choice(random_choices)
-                                   for x in range(2))
+        logger.warning('Nick %s is already in use', state.nick)
 
-            return '{}_{}'.format(nick, random_chars)
-
-        nick = state.settings.irc.nick
-
-        logger.warning('Nick %s is already in use', nick)
-
-        new_nick = new_nick_proposal(nick)
-        state.irc.nick = new_nick
-
-        return Message(command='NICK', arguments=[new_nick])
+        state.nick = generate_nick(settings.nick)
+        return Message(command='NICK', arguments=[state.nick])
     elif message.command == '001':
-        state.irc.nick = message.arguments[0]
-        state.irc.registered = True
-    elif message.command == 'JOIN' and get_nick(message) == state.irc.nick:
-        state.irc.joined = True
+        state.nick = message.arguments[0]
+        state.registered = True
+    elif message.command == 'JOIN' and get_nick(message) == state.nick:
+        state.joined = message.arguments[0]
 
 
-def get_message(sock, state):
+def get_message(sock, settings, state):
     raw_message = sock.read()
     if not raw_message:
-        if state.settings.irc.channel and not state.irc.joined \
-           and state.irc.registered:
+        if settings.channel != state.joined:
             send_message(sock, Message(command='JOIN',
-                                       arguments=[state.settings.irc.channel]))
+                                       arguments=[settings.channel]))
         return None
 
     message = Message(raw_message=raw_message)
 
-    response = manage(message, state)
+    response = manage(message, settings, state)
     if response:
         send_message(sock, response)
         return None
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import json
 import logging
 
 from contextlib import contextmanager
+from time import sleep
 
-from state import State, from_dict, to_dict
+from state import State, from_dict
 
 import irc
 import network
@@ -12,54 +13,34 @@ import network
 logger = logging.getLogger(__name__)
 
 
-@contextmanager
-def log_exceptions():
-    try:
-        yield
-    except Exception as e:
-        # TODO: Make new logger so that the right module name is used.
-        logger.error(e)
-
-
-@contextmanager
-def maintain_connection(state):
-    class SocketNotCreatedError(Exception):
-        pass
-
-    try:
-        if not state.sock:
-            raise SocketNotCreatedError
-
-        yield state.sock
-    except (BrokenPipeError, ConnectionResetError, SocketNotCreatedError):
-        state.sock = create_irc_socket(state)
-        irc.init(state.sock, state)
-        yield state.sock
-
-
 def load_settings():
     with open('settings.json', 'r') as conf:
         return from_dict(json.load(conf))
 
 
-def create_irc_socket(state):
-    args = [state.settings.irc.host, state.settings.irc.port]
-    kwargs = {'ssl': state.settings.irc.ssl}
-    return network.BufferedSocket(*args, **kwargs)
-
-
-def init():
-    # set up logging
-    logging.basicConfig(level=logging.DEBUG)
-
-    state = State()
-    state.settings = load_settings()
-    state.sock = None
+def run_bot(state):
+    sock = None
 
     while True:
-        with log_exceptions(), maintain_connection(state) as sock:
-            irc.get_message(sock, state)
+        try:
+            if not sock:
+                sock = irc.create_socket(state.settings.irc)
+                irc.hello(sock, state.settings.irc, state.irc)
+
+            irc.get_message(sock, state.settings.irc, state.irc)
+        except (BrokenPipeError, ConnectionResetError) as e:
+            socket = None
+            sleep(30)
+        except (ConnectionAbortedError, ConnectionRefusedError) as e:
+            logger.error('Connection refused by the server, quitting...')
+            return
+        #except Exception as e:
+        #    logger.error(e)
 
 
 if __name__ == '__main__':
-    init()
+    logging.basicConfig(level=logging.DEBUG)
+    state = State()
+    state.settings = load_settings()
+    run_bot(state)
+
